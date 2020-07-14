bdccd6ad8f692f7fbc63bb43619d335cd33d13be
Celeste J Bembry <CelesteJBembry@gmail.com>
2020-07-13 22:24:26 -0500

    fixed bug from index file

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+//import logo from './img/logo.svg';
+//import './App.css';
+//import Card from "./components/Card";
+import projects from "./projects.json";
+//import Nav from "./components/Nav";
+//import Footer from "./components/Footer";
+//import Header from "./components/Header";
+//import {Col, Row, Container, Wrapper } from "./components/Grid";
+
+class App extends Component {
+  state = {
+    projects
+  };
+
+  render() {
+    return (
+      <div>
+"hello"
+</div>
+    );
+  }
+}
+
+export default App;
